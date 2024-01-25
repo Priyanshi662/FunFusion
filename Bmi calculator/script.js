6bfcc5f7551bf34a459f796e7b12f1b24dc7048e
Aashi19234 <aasthaj375@gmail.com>
2024-01-25 16:53:36 +0530

    Added BMI Calculator

---
+++ Bmi calculator/script.js
@@ -0,0 +1,25 @@
+function calculateBMI() {
+    var weight = document.getElementById("weight").value;
+    var height = document.getElementById("height").value;
+
+    if (weight !== "" && height !== "") {
+        var bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
+        var result = "Your BMI is: " + bmi;
+
+        if (bmi < 18.5) {
+            result += " - Underweight";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            result += " - Normal weight";
+        } else if (bmi >= 25 && bmi < 30) {
+            result += " - Overweight";
+        } else {
+            result += " - Obese";
+        }
+
+        document.getElementById("result").innerHTML = result;
+    } else {
+        alert("Please enter both weight and height.");
+    }
+}
+
+
